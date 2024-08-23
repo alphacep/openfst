18e94e63870ebcf79ebb42b7035cd3cb626ec090
Nickolay Shmyrev <nshmyrev@gmail.com>
2024-08-23 16:17:27 +0200

    Fix name

--- src/include/fst/bi-table.h
+++ src/include/fst/bi-table.h
@@ -327,7 +327,7 @@ class VectorHashBiTable {
   }
 
   VectorHashBiTable(const VectorHashBiTable<I, T, S, FP, H, HS> &table)
-      : selector_(table.s_),
+      : selector_(table.selector_),
         fp_(table.fp_),
         h_(table.h_),
         id2entry_(table.id2entry_),
